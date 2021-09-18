c95b94483426587da81aee080a5103308c8b13e9
Yazid Elcn <elcouhenyazid@gmail.com>
2021-09-18 13:28:43 +0100

    exploring session scope with httpSession and sessionAttributes from springmvc

--- springmvc-scopes/src/main/java/com/elcnyazid/contorllers/SessionScopeAttributeController.java
+++ springmvc-scopes/src/main/java/com/elcnyazid/contorllers/SessionScopeAttributeController.java
@@ -1,5 +1,39 @@
 package com.elcnyazid.contorllers;
 
-public class SessionScopeAttributeController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@RequestMapping("/session")
+@SessionAttributes({ "Login" })
+public class SessionScopeAttributeController {
+	@GetMapping("/")
+	// we still get access to Login because it's in the session scope
+	public String setSessionAttributes(Model model, HttpSession session) {
+		model.addAttribute("Login", "YazidEl");
+		model.addAttribute("other", " Irrelevant");
+		session.setAttribute("Login", "Can't override");
+		session.setAttribute("Pass", "Dummy123");
+		// When you use HttpSession to add Session Attribute, it will add immedidately
+		// when you use @SessionAttribute to add attribute, it will add just before the
+		// page rendering,
+		// before the current request ends
+		//session.invalidate(); -> Remove all attribute in httpSession 
+		//session.removeAttribute("Login");
+		return "redirect:/session/display";
+	}
+
+	@GetMapping("/display")
+	public String displaySession(SessionStatus status) {
+		 //Only remove attributes set by using @SessionAttribute
+        //It removes attributes from session scope after the page rendering
+        status.setComplete();
+		return "display";
+	}
 }
